5f7262e044199f20fbff1b6bab237c91822f10d7
stagnate6628 <119753157+stagnate6628@users.noreply.github.com>
2023-02-09 18:59:29 +0000

    feat: 2t1 deps

---
+++ Dependencies/[SPH] 2Take1.lua
@@ -0,0 +1,18 @@
+local footer_path = filesystem.resources_dir() .. 'stand-profile-helper\\2Take1\\Footer.bmp'
+
+if not filesystem.is_regular_file(footer_path) then
+		util.toast('[SPH] Could not find footer, you may need to manually download this file.')
+    return
+end
+
+local footer = directx.create_texture(filesystem.resources_dir() .. 'stand-profile-helper\\2Take1\\Footer.bmp')
+
+while true do
+    if menu.is_open() then
+        local x, y, w, h = menu.get_main_view_position_and_size()
+        directx.draw_texture(footer, 1, (31 / 1080) / 2, 0, 0, x - 0.01 / 1920, (y + h - (1 / 1080)), 0, 1, 1, 1, 1)
+    end
+    util.yield()
+end
+
+util.keep_running()
