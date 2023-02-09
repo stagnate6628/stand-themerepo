8e39b6e90d4480bfed0a7c742d8f0c552e1d6d6b
stagnate6628 <119753157+stagnate6628@users.noreply.github.com>
2023-02-08 17:36:31 -0800

    fix: remove debug lib

--- profile_helper.lua
+++ profile_helper.lua
@@ -1,4 +1,4 @@
-local inspect = require("inspect")
+--local inspect = require("inspect")
 local texture_names<const> = {"Disabled", "Edit", "Enabled", "Friends", "Header Loading", "Link", "List", "Search",
                               "Toggle Off Auto", "Toggle Off", "Toggle On Auto", "Toggle On", "User", "Users"}
 local tag_names<const> = {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14",
