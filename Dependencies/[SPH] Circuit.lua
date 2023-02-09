e5b8b87282bb2860822a27848e0a6e02dfe6dda1
stagnate6628 <119753157+stagnate6628@users.noreply.github.com>
2023-02-09 22:36:23 +0000

    fix(circuit): draw all textures

--- Dependencies/[SPH] Circuit.lua
+++ Dependencies/[SPH] Circuit.lua
@@ -1,25 +1,36 @@
+local header_path = filesystem.resources_dir() .. 'stand-profile-helper\\Circuit\\Header.bmp'
 local footer_path = filesystem.resources_dir() .. 'stand-profile-helper\\Circuit\\Footer.bmp'
--- local subheader_path = filesystem.resources_dir() .. 'stand-profile-helper\\Circuit\\Subheader.bmp'
+local subheader_path = filesystem.resources_dir() .. 'stand-profile-helper\\Circuit\\Subheader.bmp'
 
 if not filesystem.is_regular_file(footer_path) then
     util.toast('[SPH] Could not find footer, you may need to manually download this file.')
-    return
+    should_exit = true
 end
 
--- if not filesystem.is_regular_file(subheader_path) then
---     util.toast('[SPH] Could not find subheader, you may need to manually download this file.')
---     return
--- end
+if not filesystem.is_regular_file(header_path) then
+    util.toast('[SPH] Could not find header, you may need to manually download this file.')
+    should_exit = true
+end
 
+if not filesystem.is_regular_file(subheader_path) then
+    util.toast('[SPH] Could not find subheader, you may need to manually download this file.')
+    should_exit = true
+end
+
+if should_exit then
+    util.stop_script()
+end
+
+local header = directx.create_texture(header_path)
 local footer = directx.create_texture(footer_path)
--- local subheader = directx.create_texture(subheader_path)
+local subheader = directx.create_texture(subheader_path)
 
 while true do
     if menu.is_open() then
         local x, y, w, h = menu.get_main_view_position_and_size()
-        -- is drawn over by the header
-        -- directx.draw_texture(subheader, 1, (33.7 / 1080) / 2, 0, 0, x, (y + h - (1 / 1080) - xy) , 0, 1, 1, 1, 1)
-        directx.draw_texture(footer, 1, (34.7 / 1080) / 2, 0, 0, (x + 32) / 1920, (y + h - (1 / 1080)), 0, 1, 1, 1, 1)
+        directx.draw_texture(header, 1, (100 / 1080) / 2, 0, 0, x, (y - (100 + 34) / 1080), 0, 1, 1, 1, 1)
+        directx.draw_texture(subheader, 1, (33.7 / 1080) / 2, 0, 0, x, (y - 34 / 1080), 0, 1, 1, 1, 1)
+        directx.draw_texture(footer, 1, (34.7 / 1080) / 2, 0, 0, x, (y + h - (1 / 1080)), 0, 1, 1, 1, 1)
     end
     util.yield()
 end
