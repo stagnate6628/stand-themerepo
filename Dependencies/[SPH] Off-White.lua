e7ab7d24a84ce0878fcab3a8bdb333838ffe8e9b
stagnate6628 <119753157+stagnate6628@users.noreply.github.com>
2023-02-22 11:38:27 -0800

    feat(deps): off-white

---
+++ Dependencies/[SPH] Off-White.lua
@@ -0,0 +1,17 @@
+local header_path = filesystem.resources_dir() .. "ProfileHelper\\Off-White\\Header.bmp"
+
+if not filesystem.is_regular_file(header_path) then
+    util.toast('[SPH] Could not find header, you may need to manually download this file.')
+    should_exit = true
+end
+
+if should_exit then
+    util.stop_script()
+end
+
+while true do
+    if menu.is_open() then
+        directx.draw_texture(header, 0.0375, 0.0375, 0.5, 0.5, 110 / 1920, 0.2491, 0, 1, 1, 1, 1)
+    end
+    util.yield()
+end
