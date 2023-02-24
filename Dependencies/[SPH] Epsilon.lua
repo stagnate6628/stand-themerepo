0c89b3552aae37d5370c3be1a3f19acfb6489260
stagnate6628 <119753157+stagnate6628@users.noreply.github.com>
2023-02-24 00:21:36 +0000

    refactor: update dep

--- Dependencies/[SPH] Epsilon.lua
+++ Dependencies/[SPH] Epsilon.lua
@@ -1,20 +1,46 @@
+local status, err = pcall(require, "downloader")
+
 local header_path = filesystem.resources_dir() .. 'ProfileHelper\\Epsilon\\Header.bmp'
 local footer_path = filesystem.resources_dir() .. 'ProfileHelper\\Epsilon\\Footer.bmp'
 local subheader_path = filesystem.resources_dir() .. 'ProfileHelper\\Epsilon\\Subheader.bmp'
 
-if not filesystem.is_regular_file(footer_path) then
-    util.toast('[SPH] Could not find footer, you may need to manually download this file.')
-    should_exit = true
+if not io.exists(header_path) then
+    if not status then
+        util.toast('[SPH] Could not find footer, you may need to manually download this file.')
+        should_exit = true
+        return
+    end
+
+    util.toast("[SPH] Header not found, attempting download. The script will automatically restart when finished.")
+    download_file("Themes/Epsilon/Header.bmp", {header_path})
+    util.toast("[SPH] Restarting")
+    util.restart_script()
 end
 
-if not filesystem.is_regular_file(header_path) then
-    util.toast('[SPH] Could not find header, you may need to manually download this file.')
-    should_exit = true
+if not io.exists(footer_path) then
+    if not status then
+        util.toast('[SPH] Could not find footer, you may need to manually download this file.')
+        should_exit = true
+        return
+    end
+
+    util.toast("[SPH] Footer not found, attempting download. The script will automatically restart when finished.")
+    download_file("Themes/Epsilon/Footer.bmp", {footer_path})
+    util.toast("[SPH] Restarting")
+    util.restart_script()
 end
 
-if not filesystem.is_regular_file(subheader_path) then
-    util.toast('[SPH] Could not find subheader, you may need to manually download this file.')
-    should_exit = true
+if not io.exists(subheader_path) then
+    if not status then
+        util.toast('[SPH] Could not find subheader, you may need to manually download this file.')
+        should_exit = true
+        return
+    end
+
+    util.toast("[SPH] Subheader not found, attempting download. The script will automatically restart when finished.")
+    download_file("Themes/Epsilon/Subheader.bmp", {subheader_path})
+    util.toast("[SPH] Restarting")
+    util.restart_script()
 end
 
 if should_exit then
