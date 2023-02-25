52436a9caf89a0f41874bb8ec8428a06f46da477
stagnate6628 <119753157+stagnate6628@users.noreply.github.com>
2023-02-25 09:08:20 -0800

    feat: re-add globe header

--- Dependencies/[SPH] Ozark.lua
+++ Dependencies/[SPH] Ozark.lua
@@ -3,6 +3,10 @@ local status, err = pcall(require, "downloader")
 local header_path = filesystem.resources_dir() .. "ProfileHelper\\Ozark\\Header.bmp"
 local subheader_path = filesystem.resources_dir() .. "ProfileHelper\\Ozark\\Subheader.bmp"
 
+local interaction_header_path = function(i)
+    return filesystem.resources_dir() .. 'ProfileHelper\\Ozark\\Interaction Header\\Header' .. i .. ".bmp"
+end
+
 if not io.exists(header_path) then
     if not status then
         util.toast("[SPH] Header not found, you may need to manually download this file.")
@@ -14,6 +18,13 @@ if not io.exists(header_path) then
     download_file("Themes/Ozark/Header.bmp", {header_path})
     util.toast("[SPH] Restarting")
     util.restart_script()
+end
+
+for i = 1, 18 do
+    if not io.exists(interaction_header_path(i)) then
+        util.toast("[SPH] Downloaded globe header " .. i .. "/18")
+        download_file("Themes/Ozark/Interaction Header/Header" .. i .. ".bmp", {interaction_header_path(i)})
+    end
 end
 
 if not io.exists(subheader_path) then
@@ -35,10 +46,25 @@ end
 
 local header = directx.create_texture(header_path)
 local subheader = directx.create_texture(subheader_path)
+local globe = directx.create_texture(interaction_header_path(1))
+
+util.create_tick_handler(function()
+    if not menu.is_open() then
+        return false
+    end
+
+    for i = 1, 18 do
+        util.yield(50)
+        globe = directx.create_texture(interaction_header_path(i))
+    end
+
+    util.yield(8 * 1000)
+end)
 
 while true do
     if menu.is_open() then
         local x, y, w, h = menu.get_main_view_position_and_size()
+        directx.draw_texture(globe, 1, w / 1080 + 0.0498, 0, 0, x, y - 145 / 1080, 0, 1, 1, 1, 1)
         directx.draw_texture(header, 1, w / 1080 + 0.0498, 0, 0, x, y - 145 / 1080, 0, 1, 1, 1, 1)
         directx.draw_texture(subheader, 1, w / 1080 + 0.01694, 0, 0, x, y - 37 / 1080, 0, 1, 1, 1, 1)
     end
