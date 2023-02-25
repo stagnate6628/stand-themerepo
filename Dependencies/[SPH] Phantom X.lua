107347f98ace494684513e6039bccc8c11a67e9c
stagnate6628 <119753157+stagnate6628@users.noreply.github.com>
2023-02-25 08:41:00 -0800

    fix: draw missing textures

--- Dependencies/[SPH] Phantom X.lua
+++ Dependencies/[SPH] Phantom X.lua
@@ -54,8 +54,9 @@ local subheader = directx.create_texture(subheader_path)
 while true do
     if menu.is_open() then
         local x, y, w, h = menu.get_main_view_position_and_size()
-        -- directx.draw_texture(header, 1, (83.3 / 1080) / 2, 0, 0, x, y - 127 / 1080, 0, 1, 1, 1, 1)
-        -- directx.draw_texture(footer, 1, (31 / 1080) / 2, 0, 0, x - 0.01 / 1920, (y + h - (1 / 1080)), 0, 1, 1, 1, 1)
+        directx.draw_texture(header, 1, (98 / 1080) / 2, 0, 0, x, y - 127 / 1080, 0, 1, 1, 1, 1)
+        directx.draw_texture(subheader, 1, (28.7 / 1000) / 2, 0, 0, x, y - 31 / 1080, 0, 1, 1, 1, 1)
+        directx.draw_texture(footer, 1, (31 / 1080) / 2, 0, 0, x - 0.01 / 1920, (y + h - (1 / 1080)), 0, 1, 1, 1, 1)
     end
     util.yield()
 end
