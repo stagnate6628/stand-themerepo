c7c407efb709e50011137c820b48e74294b2213b
stagnate6628 <119753157+stagnate6628@users.noreply.github.com>
2023-03-08 16:14:54 -0800

    feat: re-add auto-updater

--- ProfileHelper.lua
+++ ProfileHelper.lua
@@ -1,5 +1,75 @@
-require('lib/ProfileHelperLib')
-local inspect = require('lib/inspect')
+-- Auto Updater from https://github.com/hexarobi/stand-lua-auto-updater
+local status, auto_updater = pcall(require, 'auto-updater')
+if not status then
+		local auto_update_complete = nil
+		util.toast('Installing auto-updater...', TOAST_ALL)
+		async_http.init('raw.githubusercontent.com', '/hexarobi/stand-lua-auto-updater/main/auto-updater.lua',
+		                function(result, headers, status_code)
+				local function parse_auto_update_result(result, headers, status_code)
+						local error_prefix = 'Error downloading auto-updater: '
+						if status_code ~= 200 then
+								util.toast(error_prefix .. status_code, TOAST_ALL)
+								return false
+						end
+						if not result or result == '' then
+								util.toast(error_prefix .. 'Found empty file.', TOAST_ALL)
+								return false
+						end
+						filesystem.mkdir(filesystem.scripts_dir() .. 'lib')
+						local file = io.open(filesystem.scripts_dir() .. 'lib\\auto-updater.lua', 'wb')
+						if file == nil then
+								util.toast(error_prefix .. 'Could not open file for writing.', TOAST_ALL)
+								return false
+						end
+						file:write(result)
+						file:close()
+						util.toast('Successfully installed auto-updater lib', TOAST_ALL)
+						return true
+				end
+				auto_update_complete = parse_auto_update_result(result, headers, status_code)
+		end, function()
+				util.toast('Error downloading auto-updater lib. Update failed to download.', TOAST_ALL)
+		end)
+		async_http.dispatch()
+		local i = 1
+		while (auto_update_complete == nil and i < 40) do
+				util.yield(250)
+				i = i + 1
+		end
+		if auto_update_complete == nil then
+				error('Error downloading auto-updater lib. HTTP Request timeout')
+		end
+		auto_updater = require('auto-updater')
+end
+if auto_updater == true then
+		error('Invalid auto-updater lib. Please delete your Stand/Lua Scripts/lib/auto-updater.lua and try again')
+end
+
+local auto_update_config = {
+		source_url = 'https://raw.githubusercontent.com/stagnate6628/stand-profile-helper/main/ProfileHelper.lua',
+		script_relpath = SCRIPT_RELPATH,
+		verify_file_begins_with = '--',
+		check_interval = 86400,
+		silent_updates = true,
+		dependencies = {{
+				name = 'ProfileHelperLib',
+				source_url = 'https://raw.githubusercontent.com/stagnate6628/stand-profile-helper/main/lib/ProfileHelperLib.lua',
+				script_relpath = 'lib/ProfileHelperLib.lua',
+				verify_file_begins_with = '-- ProfileHelperLib.lua ',
+				is_required = true
+		}}
+}
+auto_updater.run_auto_update(auto_update_config)
+for _, dependency in auto_update_config.dependencies do
+		if dependency.is_required then
+				if dependency.loaded_lib == nil then
+						util.toast('Error loading lib ' .. dependency.name, TOAST_ALL)
+				else
+						local var_name = dependency.name
+						_G[var_name] = dependency.loaded_lib
+				end
+		end
+end
 
 local texture_names<const> = table.freeze({'Disabled.png', 'Edit.png', 'Enabled.png', 'Font.spritefont', 'Friends.png',
                                            'Header Loading.png', 'Link.png', 'List.png', 'Search.png', 'Toggle Off Auto.png',
@@ -402,7 +472,7 @@ local function download_headers(update)
 								end)
 
 								if math.random() > 0.5 then
-									util.toast('Make sure to save the current profile to load the Custom Header on start.')
+										util.toast('Make sure to save the current profile to load the Custom Header on start.')
 								end
 						end)
 
