0f4b17d99f2c133869f94ea97ccbf1065e27dd90
stagnate6628 <119753157+stagnate6628@users.noreply.github.com>
2023-03-18 09:58:25 -0700

    refactor(lib): improve downloading

--- lib/ProfileHelperLib.lua
+++ lib/ProfileHelperLib.lua
@@ -1,152 +1,154 @@
 -- ProfileHelperLib.lua 
 local function handle_ratelimit(status_code)
-		if status_code == 403 then
-				util.toast('You are currently ratelimited by Github. You can let it expire or a use a vpn.')
-				return
-		end
+  if status_code == 403 then
+    util.toast('You are currently ratelimited by Github. You can let it expire or a use a vpn.')
+    return
+  end
 end
 local function is_404(status_code)
-		return status_code == 404
+  return status_code == 404
 end
 -- https://stackoverflow.com/questions/9102126/lua-return-directory-path-from-path
 local function get_dirname_from_path(path)
-		if type(path) ~= 'string' then
-				return nil
-		end
+  if type(path) ~= 'string' then
+    return nil
+  end
 
-		return path:match('(.*[/\\])')
+  return path:match('(.*[/\\])')
 end
 local function write_file(path, body)
-		io.makedirs(get_dirname_from_path(path))
+  io.makedirs(get_dirname_from_path(path))
 
-		local file = io.open(path, 'wb')
-		file:write(body)
-		file:close()
+  local file = io.open(path, 'wb')
+  file:write(body)
+  file:close()
 end
 local function get_github_auth()
-		local file = io.open(filesystem.resources_dir() .. 'ProfileHelper\\.github', 'r')
-		if file == nil then
-			return nil
-		end
-		local token = file:read('a')
-		file:close()
+  local file = io.open(filesystem.resources_dir() .. 'ProfileHelper\\.github', 'r')
+  if file == nil then
+    return nil
+  end
+  local token = file:read('a')
+  file:close()
 
-		if type(token) == 'string' and token:startswith('ghp_') and token:len() == 40 then
-				return token
-		end
+  if type(token) == 'string' and token:startswith('ghp_') and token:len() == 40 then
+    return token
+  end
 
-		return nil
+  return nil
 end
 
 lib = {}
 
 function lib:download_file(url_path, file_path, on_success, on_fail, on_not_found)
-		local downloading = true
+  local downloading = true
 
-		async_http.init('https://raw.githubusercontent.com', '/stagnate6628/stand-profile-helper/main/' .. url_path,
-		                function(body, headers, status_code)
-				handle_ratelimit(status_code)
+  async_http.init('https://raw.githubusercontent.com', '/stagnate6628/stand-profile-helper/main/' .. url_path,
+      function(body, headers, status_code)
+        handle_ratelimit(status_code)
 
-				if is_404(status_code) then
-						pcall(on_not_found)
-						goto continue
-				end
+        if is_404(status_code) then
+          pcall(on_not_found)
+          goto continue
+        end
 
-				if type(file_path) == 'string' then
-						file_path = {file_path}
-				end
+        if type(file_path) == 'string' then
+          file_path = {file_path}
+        end
 
-				if type(file_path) == 'table' and #file_path > 0 then
-						for _, path in file_path do
-								write_file(path, body)
-								pcall(on_success, body, headers, status_code)
-						end
-				end
-				::continue::
-				downloading = false
-		end, function()
-				downloading = false
-				pcall(on_fail)
-		end)
-		local token = get_github_auth()
-		if token then
-				async_http.add_header('Authorization', 'Bearer ' .. token)
-		end
+        if type(file_path) == 'table' and #file_path > 0 then
+          for _, path in file_path do
+            write_file(path, body)
+          end
+          pcall(on_success, body, headers, status_code)
+        end
+        ::continue::
+        downloading = false
+      end, function()
+        downloading = false
+        pcall(on_fail)
+      end)
+  local token = get_github_auth()
+  if token then
+    async_http.add_header('Authorization', 'Bearer ' .. token)
+  end
 
-		async_http.dispatch()
+  async_http.dispatch()
 
-		while downloading do
-				util.yield()
-		end
+  while downloading do
+    util.yield()
+  end
 end
 
 function lib:make_request(url_path, callback)
-		local downloading = true
-		async_http.init('https://api.github.com', '/repos/stagnate6628/stand-profile-helper/contents/' .. url_path,
-		                function(body, headers, status_code)
-				downloading = false
-				handle_ratelimit(status_code)
+  local downloading = true
+  async_http.init('https://api.github.com', '/repos/stagnate6628/stand-profile-helper/contents/' .. url_path,
+      function(body, headers, status_code)
+        downloading = false
+        handle_ratelimit(status_code)
 
-				callback(body, headers, status_code)
-		end)
+        callback(body, headers, status_code)
+      end)
 
-		local token = get_github_auth()
-		if token then
-				async_http.add_header('Authorization', 'Bearer ' .. token)
-		end
+  local token = get_github_auth()
+  if token then
+    async_http.add_header('Authorization', 'Bearer ' .. token)
+  end
 
-		async_http.dispatch()
+  async_http.dispatch()
 
-		while downloading do
-				util.yield()
-		end
+  while downloading do
+    util.yield()
+  end
 end
 
 function lib:copy_file(from, to)
-		io.copyto(from, to)
+  io.copyto(from, to)
 end
 
 function lib:empty_dir(dir)
-		for _, path1 in io.listdir(dir) do
-				if io.isfile(path1) then
-						io.remove(path1)
-				end
+  for _, path1 in io.listdir(dir) do
+    if io.isfile(path1) then
+      io.remove(path1)
+    end
 
-				if io.isdir(path1) then
-						for _, path2 in io.listdir(path1) do
-								io.remove(path2)
-						end
-				end
-		end
+    if io.isdir(path1) then
+      for _, path2 in io.listdir(path1) do
+        io.remove(path2)
+      end
+
+      io.remove(path1)
+    end
+  end
 end
 
 function lib:get_ext(file_name)
-		local split = string.split(file_name, '.')
-		return split[#split]
+  local split = string.split(file_name, '.')
+  return split[#split]
 end
 
 function lib:trigger_command(command, args)
-		local input = command
-		if args then
-				input = command .. ' ' .. args
-		end
+  local input = command
+  if args then
+    input = command .. ' ' .. args
+  end
 
-		menu.trigger_commands(input)
+  menu.trigger_commands(input)
 end
 
 function lib:trigger_command_by_ref(path, args)
-		local ref = menu.ref_by_path(path, 44)
-		if not ref:isValid() then
-				return false
-		end
+  local ref = menu.ref_by_path(path, 44)
+  if not ref:isValid() then
+    return false
+  end
 
-		if args == nil then
-				menu.trigger_command(ref)
-		else
-				menu.trigger_command(ref, args)
-		end
+  if args == nil then
+    menu.trigger_command(ref)
+  else
+    menu.trigger_command(ref, args)
+  end
 
-		return true
+  return true
 end
 
 return lib
