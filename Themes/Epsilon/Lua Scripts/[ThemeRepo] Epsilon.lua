c234e9335a5897812ef1953b5e263b05323b23f2
stagnate6628 <119753157+stagnate6628@users.noreply.github.com>
2023-05-09 19:19:07 -0700

    fix: epsilon theme lua

--- Themes/Epsilon/Lua Scripts/[ThemeRepo] Epsilon.lua
+++ Themes/Epsilon/Lua Scripts/[ThemeRepo] Epsilon.lua
@@ -1,14 +1,15 @@
-local header_path = filesystem.resources_dir() .. 'ThemeRepo\\Themes\\Epsilon\\Header.bmp'
-local footer_path = filesystem.resources_dir() .. 'ThemeRepo\\Themes\\Epsilon\\Footer.bmp'
-local subheader_path = filesystem.resources_dir() .. 'ThemeRepo\\Themes\\Epsilon\\Subheader.bmp'
+local dir = filesystem.resources_dir()
+local header_path = dir .. 'ThemeRepo\\Themes\\Epsilon\\Header.bmp'
+local footer_path = dir .. 'ThemeRepo\\Themes\\Epsilon\\Footer.bmp'
+local subheader_path = dir .. 'ThemeRepo\\Themes\\Epsilon\\Subheader.bmp'
 
-local interaction_header_path = function(i)
-	return filesystem.resources_dir() .. 'ThemeRepo\\Themes\\Epsilon\\Interaction Header\\Header' .. i .. '.bmp'
+local function get_header_path(i)
+	return dir .. $'ThemeRepo\\Themes\\Epsilon\\Interaction Header\\Header{i}.bmp'
 end
 
 for i = 1, 18 do
-	if not filesystem.exists(interaction_header_path(i)) then
-		util.toast('[ThemeRepo] Interaction header ' .. i .. ' not found!')
+	if not filesystem.exists(get_header_path(i)) then
+		util.toast($'[ThemeRepo] Interaction header {i} not found!')
 		util.stop_script()
 	end
 end
@@ -23,22 +24,26 @@ end
 local header = directx.create_texture(header_path)
 local footer = directx.create_texture(footer_path)
 local subheader = directx.create_texture(subheader_path)
-local globe = directx.create_texture(interaction_header_path(1))
+local globe = directx.create_texture(get_header_path(1))
 
 util.create_tick_handler(function()
 	if menu.is_open() then
 		for i = 1, 18 do
-			--util.yield(50)
-			globe = directx.create_texture(interaction_header_path(i))
+			util.yield(50)
+			globe = directx.create_texture(get_header_path(i))
 		end
-	
+		util.yield(8 * 1000)
+	end
+	return true
+end)
+
+util.create_tick_handler(function()
+	if menu.is_open() then	
 		local x, y, w, h = menu.get_main_view_position_and_size()
 		directx.draw_texture(globe, 1, w / 1080 + 0.04984, 0, 0, x, y - 130 / 1080, 0, 1, 1, 1, 1)
 		directx.draw_texture(header, 1, w / 1080 + 0.04984, 0, 0, x, y - 130 / 1080, 0, 1, 1, 1, 1)
 		directx.draw_texture(subheader, 1, w / 1080 + 0.01274, 0, 0, x, y - 28 / 1080, 0, 1, 1, 1, 1)
 		directx.draw_texture(footer, 1, w / 1080 + 0.01368, 0, 0, x, y + h - (1 / 1080) + 0.0013, 0, 1, 1, 1, 1)
-
-		util.yield(8 * 1000)
 	end
 	return true
 end)
