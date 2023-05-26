821b775674f97be8d3bee5e4df7edc8866a86b99
stagnate6628 <119753157+stagnate6628@users.noreply.github.com>
2023-05-26 14:30:57 -0700

    refactor: cleanup

--- Themes/Rebound/Lua Scripts/[ThemeRepo] Rebound.lua
+++ Themes/Rebound/Lua Scripts/[ThemeRepo] Rebound.lua
@@ -16,8 +16,6 @@ local subheader = directx.create_texture(subheader_path)
 util.create_tick_handler(function()
 	if menu.is_open() then
 		local x, y, w, h = menu.get_main_view_position_and_size()
-		local curr = menu.get_current_menu_list()
-		local cursor_text = menu.get_active_list_cursor_text(true, false)
 
 		directx.draw_texture(header, 1, w / 1080 + 0.047, 0, 0, x, y - 137 / 1080, 0, 1, 1, 1, 1)
 		directx.draw_texture(subheader, 1, w / 1080 + 0.015979, 0, 0, x, y - 35 / 1080, 0, 1, 1, 1, 1)
@@ -25,7 +23,7 @@ util.create_tick_handler(function()
 
 		directx.draw_text(
 			x + 0.003, y - 0.025,
-			lang.get_string(curr.menu_name):upper(),
+			lang.get_string(menu.get_current_menu_list().menu_name):upper(),
 			ALIGN_TOP_LEFT,
 			0.6,
 			1,
@@ -35,7 +33,7 @@ util.create_tick_handler(function()
 		)
 		directx.draw_text(
 			x + 0.235, y - 0.025,
-			table.concat(cursor_text:split('/'), ' / '),
+			table.concat(menu.get_active_list_cursor_text(true, false):split('/'), ' / '),
 			ALIGN_TOP_RIGHT,
 			0.6,
 			1,
