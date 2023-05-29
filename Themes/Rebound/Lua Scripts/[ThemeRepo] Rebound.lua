5f33eb5075ee3f310cc1856349b2d8a0fb839ef4
stagnate6628 <119753157+stagnate6628@users.noreply.github.com>
2023-05-28 21:39:45 -0700

    refactor(themes): comment out future changes

--- Themes/Rebound/Lua Scripts/[ThemeRepo] Rebound.lua
+++ Themes/Rebound/Lua Scripts/[ThemeRepo] Rebound.lua
@@ -21,26 +21,26 @@ util.create_tick_handler(function()
 		directx.draw_texture(subheader, 1, w / 1080 + 0.015979, 0, 0, x, y - 35 / 1080, 0, 1, 1, 1, 1)
 		directx.draw_texture(footer, 1, w / 1080 + 0.01598, 0, 0, x, y + h - 1 / 1080, 0, 1, 1, 1, 1)
 
-		directx.draw_text(
-			x + 0.003, y - 0.025,
-			lang.get_string(menu.get_current_menu_list().menu_name):upper(),
-			ALIGN_TOP_LEFT,
-			0.6,
-			1,
-			1,
-			1,
-			1
-		)
-		directx.draw_text(
-			x + 0.235, y - 0.025,
-			table.concat(menu.get_active_list_cursor_text(true, false):split('/'), ' / '),
-			ALIGN_TOP_RIGHT,
-			0.6,
-			1,
-			1,
-			1,
-			1
-		)
+		-- directx.draw_text(
+		-- 	x + 0.003, y - 0.025,
+		-- 	lang.get_string(menu.get_current_menu_list().menu_name):upper(),
+		-- 	ALIGN_TOP_LEFT,
+		-- 	0.6,
+		-- 	1,
+		-- 	1,
+		-- 	1,
+		-- 	1
+		-- )
+		-- directx.draw_text(
+		-- 	x + 0.235, y - 0.025,
+		-- 	table.concat(menu.get_active_list_cursor_text(true, false):split('/'), ' / '),
+		-- 	ALIGN_TOP_RIGHT,
+		-- 	0.6,
+		-- 	1,
+		-- 	1,
+		-- 	1,
+		-- 	1
+		-- )
 	end
 	return true
 end)
